994702e453b6c208663c8a1b61cd425778a6a183
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2014-08-22 19:30:13 +0200

    fixes #6 - one set of credentials per user for now

--- mainpage.py
+++ mainpage.py
@@ -8,45 +8,67 @@ from google.appengine.ext import ndb
 from google.appengine.api import users
 
 class TogglCredential(ndb.Model):
-	user = ndb.UserProperty()
-	api_token = ndb.StringProperty()
-	workspace_id = ndb.IntegerProperty()
+    api_token = ndb.StringProperty()
+    workspace_id = ndb.IntegerProperty()
 
 MAINPAGE_TEMPLATE = """
-	<html>
-		<body>
-			<h2>Enter your details</h2>
-			<form action="/store" method="post">
-				toggl API token: 
-				<input name="toggl_api_token">
-				toggl workspace ID: 
-				<input name="workspace_id">
-				<input type="submit" value="Submit">
-				<br>
-				Logged in as %s
-			</form>
-		</body>
-	</html>
+    <html>
+        <body>
+            <h2>Enter your details</h2>
+            <form action="/store" method="post">
+                toggl API token: 
+                <input name="toggl_api_token">
+                toggl workspace ID: 
+                <input name="workspace_id">
+                <input type="submit" value="Submit">
+                <br>
+                Logged in as %s         
+            </form>
+            <br>
+            <form action="/toggldata method="get">
+                <input type="button" value"Get Data">
+            </form>
+        </body>
+    </html>
 """
 
 class MainPage(webapp2.RequestHandler):
-	def get(self):
-		self.response.headers['content-type'] = 'text/html'
-		self.response.write(MAINPAGE_TEMPLATE % str(users.get_current_user()))
+    def get(self):
+        """ Create credential entry page using the template """
+        self.response.headers['content-type'] = 'text/html'
+        self.response.write(MAINPAGE_TEMPLATE % str(users.get_current_user()))
 
 
 class Store(webapp2.RequestHandler):
-	def post(self):
-		togglCredential = TogglCredential()
-		togglCredential.user = users.get_current_user()
-		togglCredential.api_token = str(self.request.get('toggl_api_token'))
-		togglCredential.workspace_id = int(self.request.get('workspace_id'))
-		togglCredential.put()
+    def post(self):
+        """ Store credentials in the DataStore """        
+        # togglCredential.key_name = users.get_current_user() # trying out putting by key
+        togglCredential = TogglCredential(id=str(users.get_current_user())) #another option - but it needs 'id' not 'key_name'
+        togglCredential.api_token = str(self.request.get('toggl_api_token'))
+        togglCredential.workspace_id = int(self.request.get('workspace_id'))
+        togglCredential.put()
 
-		self.response.write(str(togglCredential.api_token) + ' ' + str(togglCredential.workspace_id) + ' ' + 'saved for user ' + str(users.get_current_user()))
+        self.response.write(str(togglCredential.api_token) + ' ' + str(togglCredential.workspace_id) + ' ' + 'saved for user ' + str(users.get_current_user()))
+
+
+class TogglData(webapp2.RequestHandler):
+    def get(self):
+        """ Get data from toggl 
+        user = users.get_current_user()
+        togglCredential = togglCredential...
+        workspace_id = ndb....
+        request = urllib2.Request("https://toggl.com/reports/api/v2/details?user_agent=andrzej.ruszczewski@gmail.com&workspace_id=%s" % workspace_id)
+        api_token = ndb...
+        base64string = base64.encodestring('%s:%s' % (api_token, 'api_token')).replace('\n', '')
+        request.add_header("Authorization", "Basic %s" % base64string)
+        data = json.load(urllib2.urlopen(request))
+        self.response.headers['content-type'] = 'application/json'
+        self.response.write(json.dumps(data, indent=4, sort_keys=True))
+        """
 
 
 application = webapp2.WSGIApplication([
-	('/', MainPage),
-	('/store', Store)
+    ('/', MainPage),
+    ('/store', Store),
+    ('/toggldata', TogglData)
 ], debug=True)
